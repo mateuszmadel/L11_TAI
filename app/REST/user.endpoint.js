e46b230bec6e6af688d07a801643bcc56988a555
Mateusz Mądel <mateuszm7234@gmail.com>
2021-05-14 16:14:32 +0200

    lab9

---
+++ app/REST/user.endpoint.js
@@ -0,0 +1,31 @@
+import business from '../business/business.container'
+const userEndpoint = (router) => {
+    router.post('/api/user/auth', async (request, response, next) => {
+        try {
+            let result = await business.getUserManager(request).authenticate(request.body.login, request.body.password);
+            response.status(200).send(result);
+        } catch (error) {
+            applicationException.errorHandler(error, response);
+        }
+    });
+
+    router.post('/api/user/create', async (request, response, next) => {
+        try {
+            const result = await business.getUserManager(request).createNewOrUpdate(request.body);
+            response.status(200).send(result);
+        } catch (error) {
+            applicationException.errorHandler(error, response);
+        }
+    });
+
+    router.delete('/api/user/logout/:userId', async (request, response, next) => {
+        try {
+            let result = await business.getUserManager(request).removeHashSession(request.body.userId);
+            response.status(200).send(result);
+        } catch (error) {
+            applicationException.errorHandler(error, response);
+        }
+    });
+};
+
+export default userEndpoint;
